f5c6ffbff320da04af77d6c677892dae23594d09
Barry Oliver <i31770@gmail.com>
2017-03-13 16:08:46 -0400

    Section 1 and 2 working.

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-from pylab import *
+from pandas import *
+from tabulate import tabulate
 
-gene_exp_1 = loadtxt(open("Gene_expression_1.csv", "r"), delimiter=",")
-adj_1 = loadtxt(open("Adj_1.csv", "r"), delimiter=",")
+# 1. Load the gene expression data (Gene_expression_1.csv) and the ground truth adjacency
+# matrix (Adj_1.csv).
+gene_exp_1 = pandas.read_csv('Gene_expression_1.csv')
+adj_1 = pandas.read_csv('Adj_1.csv')
+
+# 2. Compute pairwise correlation matrix, and show the matrix. see Fig. 1
+print(tabulate(adj_1))
+rowIDs = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+corr = gene_exp_1.corr('pearson')
+corr_matrix = corr.as_matrix()
+print(tabulate(corr_matrix, showindex=rowIDs, headers=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]))
+
+# 3. Given the range of threshold (e.g., 0, 0.1, 0.2, 0.3, …, 0.9, 1), compare the adjacency
+th_corr = corr.clip_lower(0.1)
+print(tabulate(th_corr.as_matrix(), showindex=rowIDs, headers=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]))
+
+# matrices between the network and the ground truth.
+
+
+# 4. Compute a confusion matrix for each threshold
+
+# 5. Compute TPR and FPR for each threshold
+
+# 6. Make a ROC plot. E.g., see Fig. 2
